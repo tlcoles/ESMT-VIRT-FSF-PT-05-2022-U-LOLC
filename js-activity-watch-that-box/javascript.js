b9b3838542e2657cbfd3fec64b683533111966dd
Tammi L. Coles <21356324+tlcoles@users.noreply.github.com>
2022-05-24 20:59:34 +0200

    Create js to change and reset elements of a box

---
+++ js-activity-watch-that-box/javascript.js
@@ -0,0 +1,16 @@
+const myBox = document.getElementById("box");
+
+function grow() {
+    myBox.style.width = "300px";
+}
+function blue() {
+    myBox.style.backgroundColor = "rgb(0,0,225)";
+    myBox.style.opacity = "1";
+
+    }
+function fade() {
+    myBox.style.opacity = ".5";
+}
+function rs() {
+    myBox.style = "height:150px; width:150px; background-color:orange; margin:25px";
+}
